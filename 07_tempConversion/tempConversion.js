97b12063f1520cdb32473ff865b58acc78488edb
zarinha <jessica_zaramelo@hotmail.com>
2022-06-10 21:39:40 +0200

    Exercise solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fhrt) {
+const celcius = (fhrt - 32) * (5/9);
+let num = celcius;
+if (num = Number.isInteger(celcius)) return celcius;
+else return Number(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+const fahrenheit = (c * (9/5)) + 32;
+let numb = fahrenheit;
+if (numb = Number.isInteger(fahrenheit)) return fahrenheit;
+else return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
