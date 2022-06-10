c9be5cf57928a3a4d5c0fa12db37415d59f1653f
zarinha <jessica_zaramelo@hotmail.com>
2022-06-10 10:23:10 +0200

    fixed to run all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // a year divisible by 4 is a leap year. However if the year is divisible by 100 and not divisible by 400, it is not a leap year.
+if ( year % 4 == 0 && year % 100 == 0 && year % 400 == 0) return true;
+// all year divisible by 4 and not divisible by 100 are leap year.
+if ( year % 4 == 0 && year % 100 !== 0 ) return true;
+else return false;
 };
 
 // Do not edit below this line
